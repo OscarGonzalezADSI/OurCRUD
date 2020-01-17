e387de88237022414ffdc99d85ff624a5b41d845
OscarGonzalez1987 <55475821+OscarGonzalez1987@users.noreply.github.com>
2020-01-17 13:48:09 -0500

    Update index.php

--- index.php
+++ index.php
@@ -7,13 +7,13 @@ $conn = Conexion();
 OurCRUD
 OscarGonzalez1987/OurCRUD is licensed under the
 
-GNU Lesser General Public License v3.0
-Permissions of this copyleft license are conditioned on making available complete
-source code of licensed works and modifications under the same license or the GNU
-GPLv3. Copyright and license notices must be preserved. Contributors provide an 
-express grant of patent rights. However, a larger work using the licensed work 
-through interfaces provided by the licensed work may be distributed under different 
-terms and without source code for the larger work.
+GNU General Public License v3.0
+Permissions of this strong copyleft license are conditioned 
+on making available complete source code of licensed works 
+and modifications, which include larger works using a licensed 
+work, under the same license. Copyright and license notices 
+must be preserved. Contributors provide an express grant of 
+patent rights.
 
     Created on : 6/01/2020, 02:59:20 PM
     Author     : oscar gonzalez
